346e0509a16d14b21eb12670f95881e80c0d7d71
Elizabeth Hahn <elizhahn87@gmail.com>
2020-12-05 13:47:59 -0700

    Wrote functions for implementation file, passed tests

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,48 @@
 
+var shelf = []
+function shelfBook(book, shelf) {
+  if (shelf.length < 3) {
+    return shelf.unshift(book);
+  }
+}
+
+function unshelfBook(title, shelf) {
+  for (var i = 0; i < shelf.length; i++) {
+    if (title === shelf[i].title) {
+       shelf.splice(i, 1)
+    }
+  }
+}
+
+function listTitles(shelf) {
+  var titles = [];
+  for(i = 0; i < shelf.length; i++) {
+    titles.push(shelf[i].title)
+  }
+   return titles.join(', ');
+}
+
+function searchShelf(shelf, title) {
+  var onShelf = false;
+  for(i = 0; i < shelf.length; i++) {
+    if (shelf[i].title === title) {
+      onShelf = true
+    } else {
+      onShelf = false;
+    }
+   }
+    if(onShelf === true) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+
+
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+   shelfBook,
+   unshelfBook,
+   listTitles,
+   searchShelf
 };
