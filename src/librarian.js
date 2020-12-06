65fd42fd853a1bcb8a30537de03fc271c77e9d59
Elizabeth Hahn <elizhahn87@gmail.com>
2020-12-05 17:56:29 -0700

    Wrote Librarian Class and passed tests

--- src/librarian.js
+++ src/librarian.js
@@ -1,7 +1,37 @@
+
+
 class Librarian {
-  constructor() {
-    
+  constructor(name, library) {
+    this.name = name;
+    this.library = library;
+  }
+  greetPatron(patronName, morning) {
+    if (morning === true) {
+    return `Good morning, ${patronName}!`;
+  } else {
+    return `Hello, ${patronName}!`;
+  }
+  }
+
+  findBook(title) {
+    var onShelf = false;
+    if (title === this.library.shelves.fantasy[0].title) {
+    onShelf = true;
+    }
+    if (onShelf === true) {
+     this.library.shelves.fantasy.splice(0, 1);
+     var response = `Yes, we have ${title}`
+    }
+    if (onShelf === false) {
+      var response = `Sorry, we do not have ${title}`
+    }
+    return response;
+  }
+  calculateLateFee(daysLate) {
+    return Math.ceil(daysLate * .25);
   }
 }
+
+
 
 module.exports = Librarian;
